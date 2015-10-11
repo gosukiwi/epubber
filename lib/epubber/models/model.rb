5bf02d94455e7c9ae2c50a79172b2b67aeb61466
Federico Ramirez <federico_r@beezwax.net>
2015-10-11 20:28:27 -0300

    Fix xhtml cleaning

--- lib/epubber/models/model.rb
+++ lib/epubber/models/model.rb
@@ -5,7 +5,7 @@ module Epubber::Models
     # Because the EPUB format always uses XHTML, let's make sure things like
     # `<br>` gets transformed to `<br />`.
     def clean_html(html)
-      Nokogiri::XML::DocumentFragment.parse(html).to_s
+      Nokogiri::HTML::DocumentFragment.parse(html).to_xhtml
     end
   end
 end
